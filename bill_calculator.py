1d42a388438e55ce990b7808305d9cbd3c0859b7
angela_z <anggiez@gmail.com>
2016-05-03 18:28:18 -0700

    first upload 2016-5-3

---
+++ bill_calculator.py
@@ -0,0 +1,10 @@
+
+def total(num_ppl,bill_amt):
+	tip = bill_amt * 0.20
+	bill_total = tip + bill_amt
+	total_per_person = bill_total / num_ppl
+	print total_per_person
+
+total (4,80)	
+
+	
---
+++ survey.py
@@ -0,0 +1,12 @@
+print "Welcome to the survey!"
+name = raw_input ("What is your name? ")
+color = raw_input ("What is your favorite color? ")
+print name + "'s", "favorite color is", color + "."
+hobby = raw_input ("What is your hobby? ")
+print name + "'s", "hobby is", hobby + "."
+movie = raw_input ("What is your favorite movie? ")
+print name + "'s", "favorite movie is", movie + "."
+travel = raw_input ("What country did you visit most recently? ")
+print name, "traveled to", travel, "most recently."
+city = raw_input ("What city were you born in? ")
+print name, "was born in", city + "."
